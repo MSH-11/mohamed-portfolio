eb7f9674b002e2d878520cd6824d41535fa36200
Mohamed Hamouda <mhamouda24@students.claremontmckenna.edu>
2024-08-16 12:54:10 -0700

    Created stair transition between pages

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -29,7 +29,23 @@ const Stairs = () => {
        * creating a staggered effect with decreasing delay for each subsequent step
        *
        */}
-       
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
     </>
   );
 };
